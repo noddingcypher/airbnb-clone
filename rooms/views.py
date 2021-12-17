962b825a2c876419bb04f0bda94f68606348aebd
goasgo <kangsj24@gmail.com>
2021-12-17 23:05:47 +0900

    URLs argument & room_detail FBV finished

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-from django.utils import timezone
-from typing import List
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 # Create your views here.
@@ -16,8 +16,10 @@ class HomeView(ListView):
     paginate_orphans = 6
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
