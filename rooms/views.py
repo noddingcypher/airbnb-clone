606757e3e3bb84d6a2f65d0afeef8db3e547c12b
goasgo <kangsj24@gmail.com>
2021-12-19 22:00:50 +0900

    Templates,Form,Url,Setup

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from . import models
 
@@ -17,9 +16,15 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+    pk_url_kwarg = "pk"
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
